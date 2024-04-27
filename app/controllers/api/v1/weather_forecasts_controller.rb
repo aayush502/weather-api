76c4f09fa99c6b29fe7a851aba2c1b041ff215d2
aayush502 <aayushhumagain23@gmail.com>
2024-04-27 21:36:10 +0545

    feat: call weather service to get the hourly forecast data

---
+++ app/controllers/api/v1/weather_forecasts_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  # V1 for version 1 of API
+  module V1
+    # get the weather forecasts data
+    class WeatherForecastsController < ApplicationController
+      def index
+        hourly_forecast = WeatherForecastService.hourly_forecast(weather_forecast_params)
+
+        if hourly_forecast.is_a?(Hash) && hourly_forecast.key?(:error)
+          render json: hourly_forecast, status: :unprocessable_entity
+        else
+          render json: hourly_forecast, status: :ok
+        end
+      end
+
+      private
+
+      def weather_forecast_params
+        params.permit(:latitude, :longitude)
+      end
+    end
+  end
+end
